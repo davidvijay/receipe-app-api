504da612e9f3aa7f23d7d26f9aa26d82e49643f4
davidvijay <davidvijay273@gmail.com>
2025-06-05 07:50:53 +0530

    configured docker compose and check to wait for db v2

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator Functions
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    
+    return x+y
+
+
+def subrtaction(x,y):
+    """subtract from y - x then return result"""
+    
+    return y-x
