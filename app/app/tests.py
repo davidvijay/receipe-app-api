2ae10340c5dca680b26e6ef21a8f2db12bc23a21
davidvijay <davidvijay273@gmail.com>
2025-06-07 12:06:11 +0530

    changes

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,15 +5,15 @@ sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-    
     def test_add_numbers(self):
         """Testing adding numbers together."""
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
+        res=calc.add(5, 6)
+        self.assertEqual(res, 11)
         
     def test_subtract_numbers(self):
         """Testing numbers with subrtaction"""
-        res = calc.subrtaction(5,6)
+        res = calc.subrtaction(5, 6)
         self.assertEqual(res, 1)
