fa0797bbdba036d45c12e4299d902b94002688b9
davidvijay <davidvijay273@gmail.com>
2025-06-07 12:15:42 +0530

    changes

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,25 +3,27 @@ Django commands to wait for db to available
 
 """
 import time
-from psycopg2 import OperationalError as pyscopg2OpError
-from django.db.utils import OperationalError
+from psycopg2 import OperationalError as pyscopg2OpError # noqa
+from django.db.utils import OperationalError # noqa
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """ Django command to wait for database"""
+    """Django command to wait for database"""
     
     def handle(self, *args, **options):
         """Entry point of command"""
         self.stdout.write("Hello")
         self.stdout.write("Waiting for database...")
-        db_up=False
+        db_up = False
         while db_up is False:
             try:
                 self.stdout.write("In try")
                 self.check(databases=["default"])
-                db_up=True
+                db_up = True
             except:
                 self.stdout.write("In except")
                 self.stdout.write("Database unavailable, Waiting 1 second...")
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS("Database available"))
+        
