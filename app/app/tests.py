aec33888e331b191e7b349cf6bb466d5b259f694
davidvijay <davidvijay273@gmail.com>
2025-06-07 12:08:28 +0530

    changes

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,9 +10,8 @@ class CalcTest(SimpleTestCase):
     """Test the calc module."""
     def test_add_numbers(self):
         """Testing adding numbers together."""
-        res=calc.add(5, 6)
+        res= calc.add(5, 6)
         self.assertEqual(res, 11)
-        
     def test_subtract_numbers(self):
         """Testing numbers with subrtaction"""
         res = calc.subrtaction(5, 6)
