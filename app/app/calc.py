3895bf841cceba5ed3f5ddbebf10d91f598a2acf
davidvijay <davidvijay273@gmail.com>
2025-06-07 11:57:21 +0530

    configuration

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,16 +1,11 @@
 """
 Calculator Functions
 """
-
-
-
 def add(x, y):
     """Add x and y and return result"""
-    
     return x+y
 
 
 def subrtaction(x,y):
     """subtract from y - x then return result"""
-    
     return y-x
